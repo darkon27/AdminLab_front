5a0c138a84a461a895c5fd071c6301790e2d62cb
darkon27 <darkon27@gmail.com>
2025-04-23 09:42:55 -0500

    20250423 muestra

---
+++ src/app/precisa/maestros/Muestra/model/Muestra.ts
@@ -0,0 +1,23 @@
+export class Muestra{
+
+    constructor(){
+
+    }
+    num?:       number;
+    IdMuestra?:       number;
+    Nombre:           string;
+    Observacion:      string;
+    Cantidad?:        number;
+    Estado?:          number;
+    FechaCreacion?:   Date;
+    FechaModificacion?:   Date;
+    UsuarioCreacion:      string;
+    UsuarioModificacion:  string;
+    IpCreacion:         string;
+    IpModificacion:     string;
+    abreviatura:        string;
+    FlgTipoEntrada?:    number;
+    pESTADOdesc:        string;
+    FlgEntrada:         string;
+  
+}
--- src/app/precisa/maestros/Muestra/view/muestra.component.ts
+++ src/app/precisa/maestros/Muestra/view/muestra.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Table } from 'jspdf-autotable';
+import { MenuItem, SelectItem } from 'primeng/api';
 import { ComponenteBasePrincipal } from '../../../../../util/ComponenteBasePrincipa';
 import { MensajeController } from '../../../../../util/MensajeController';
 import { UIMantenimientoController } from '../../../../../util/UIMantenimientoController';
+import { Muestra } from '../model/Muestra';
+import { MuestraService } from '../service/MuestraServicio';
 import { MuestrasMantenimientoComponent } from './components/muestra-mantenimiento.component';
 
 @Component({
@@ -11,8 +15,28 @@ import { MuestrasMantenimientoComponent } from './components/muestra-mantenimien
 })
 export class MuestraComponent extends ComponenteBasePrincipal implements OnInit, UIMantenimientoController {
   @ViewChild(MuestrasMantenimientoComponent, { static: false }) muestraMantenimientoComponent: MuestrasMantenimientoComponent;
+  @ViewChild(Table, { static: false }) dataTableComponent: Table;
+  bloquearPag: boolean;
+  filtro: Muestra = new Muestra();
+  lsttipoAdmision: any[] = [];
+  lstEstado: SelectItem[] = [];
+  ltsExportar: MenuItem[];
+  verMantPersona: boolean = false;
+  validarAccion: string;
+  acciones: string = ''
+  position: string = 'top'
+  titulo: string; 
+  registroSeleccionado: any;
+  loading: boolean;
 
-  constructor() {super(); }
+  constructor(
+    private messageService: MessageService,
+    private exportarService: ExportarService,
+    private MuestraService: MuestraService,
+    private confirmationService: ConfirmationService) {
+    super();
+  }
+
   coreMensaje(mensage: MensajeController): void {
     throw new Error('Method not implemented.');
   }
@@ -26,7 +50,19 @@ export class MuestraComponent extends ComponenteBasePrincipal implements OnInit,
     this.openNew();
   }
   coreBuscar(): void {
-    throw new Error('Method not implemented.');
+    if (!this.estaVacio(this.filtro.MosDescripcion)) {
+      this.filtro.MosDescripcion = this.filtro.MosDescripcion.trim();
+    }
+    this.bloquearPag = true;
+    this.ModeloServicioService.ListarModeloServicio(this.filtro).then((res) => {
+        this.bloquearPag = false;
+        var contado = 1;
+        res.forEach(element => {
+          element.num = contado++;
+        });
+        this.lstModeloServicio = res;
+        console.log("coreBuscar listado:", res);
+      });
   }
   coreGuardar(): void {
     throw new Error('Method not implemented.');
