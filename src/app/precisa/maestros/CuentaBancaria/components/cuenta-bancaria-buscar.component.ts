c736222e435d2609d3a43936d3762e04713d5122
GaSMC21 <geampier.smc@gmail.com>
2025-06-03 11:34:43 -0500

    resolucion HS_AD-136

--- src/app/precisa/maestros/CuentaBancaria/components/cuenta-bancaria-buscar.component.ts
+++ src/app/precisa/maestros/CuentaBancaria/components/cuenta-bancaria-buscar.component.ts
@@ -4,47 +4,47 @@ import { ComponenteBasePrincipal } from "../../../../../util/ComponenteBasePrinc
 
 
 @Component({
-    selector: 'ngx-cuenta-bancaria-buscar',
-    templateUrl: './cuenta-bancaria-buscar.component.html'
-  })
+  selector: 'ngx-cuenta-bancaria-buscar',
+  templateUrl: './cuenta-bancaria-buscar.component.html'
+})
 export class CuentaBancariaBuscarComponent extends ComponenteBasePrincipal implements OnInit {
 
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
-    verdocumento: boolean = false;
-    vernombre: boolean = true;
-    acciones: string = ''
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  verdocumento: boolean = false;
+  vernombre: boolean = true;
+  acciones: string = ''
   position: string = 'top'
-  dialog:boolean=false;
-  iniciarComponente(accion: string,titulo) {
-    this.dialog=true;
+  dialog: boolean = false;
+  iniciarComponente(accion: string, titulo) {
+    this.dialog = true;
     // if (accion == "NUEVO") {
-      this.cargarAcciones(accion,titulo)
+    this.cargarAcciones(accion, titulo)
 
-    
+
     // else{
     //   this.cargarAcciones(accion,titulo)
     // }
-    
+
   }
 
-  checknombre(hola:any){
+  checknombre(hola: any) {
     //console.log("entro",hola);
     this.vernombre = true;
     this.verdocumento = false;
 
   }
-  checkdocumento(anyd:any){
+  checkdocumento(anyd: any) {
     this.vernombre = false;
     this.verdocumento = true;
   }
-  
-  cargarAcciones(accion: string,titulo) {
+
+  cargarAcciones(accion: string, titulo) {
     this.acciones = `${titulo}: ${accion}`;
-   
-      this.dialog = true;
-      this.puedeEditar = false;
-    
+
+    this.dialog = true;
+    this.puedeEditar = false;
+
   }
 }
